0c55bce6ec24cc94499bf295967a7a6fa28b66f1
Atanas Pozharliev <abpozharliev19@codingburgas.bg>
2021-03-26 16:43:09 +0200

    Add the data and start analyzing it. Fix #7

--- Data Manipulation/Data Analysis/main.py
+++ Data Manipulation/Data Analysis/main.py
@@ -1,8 +1,20 @@
-import numpy
+import numpy as np
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
 
 def main():
-    pass
+    plt.style.use('bmh')
+    # Store the data
+    col_list = [ "average_water_level"]
+    data_store = pd.read_csv('../../Data/tcc-water-dam-levels.csv', usecols=col_list)
+    print(data_store)
+
+   
+
 
 if __name__ == "__main__":
     main()
