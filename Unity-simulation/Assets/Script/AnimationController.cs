4baba92d3a97cdb28275e56f5172220278d89d0a
Stoyan Ivanov <ssivanov19@codingburgas.bg>
2021-03-26 12:02:48 +0200

    Add more functionals for Unity simulation. Fix #4, fix #3, fix #2

---
+++ Unity-simulation/Assets/Script/AnimationController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private bool isDamRaise = true;
+    
+    public Animator damAnimator;
+    public Animator riverAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space was pressed!");
+            if (isDamRaise)
+            {
+                damAnimator.SetTrigger("Low");
+                riverAnimator.SetTrigger("Raise");
+                isDamRaise = false;
+            }
+            else
+            {
+                damAnimator.SetTrigger("Raise");
+                riverAnimator.SetTrigger("Low");
+                isDamRaise = true;
+            }
+        }
+    }
+}
